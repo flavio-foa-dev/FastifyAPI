d5d1da01180035c36f5a1789f1db66d70988a93b
Flavio Andrade <foa12381@gmail.com>
2023-10-31 14:24:39 -0300

    fix/.env

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { env } from './env';
 app.listen({
   port: env.PORT
 }).then(() => {
-  console.log('listening on port 3333');
+  console.log('listening on port', env.PORT);
 }).catch((error) => {
   console.log('error: ', error.message);
 });
